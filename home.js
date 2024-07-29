c8be55acd7bb267f39fddd69b667653147122870
유재신 <69245660+ryoo0930@users.noreply.github.com>
2024-07-29 20:49:59 +0900

    디자인 수정 및 기능 확대

--- home.js
+++ home.js
@@ -1,9 +1,32 @@
-
 const se = document.createElement('script');
 se.src = 'https://ipinfo.io?callback=callback';
 document.body.appendChild(se);
-document.body.removeChild(se);
 
+// 콜백 함수 정의
 function callback(data) {
-    document.getElementById('client-ip').textContent = data.ip;
+     const elements = document.querySelectorAll('.client-ip');
+    elements.forEach((element) => {
+        element.textContent = data.ip;
+    });
 }
+
+// 스크립트를 제거하기 위해 콜백 함수가 실행된 후에 실행될 수 있도록 설정
+se.onload = () => {
+    document.body.removeChild(se);
+};
+
+document.getElementById('messageInput').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        const input = document.getElementById('messageInput');
+        const newText = input.value;
+
+        if (newText) {
+            const newDiv = document.createElement('div');
+            newDiv.className = 'inputCHAT';
+            newDiv.textContent = newText;
+            document.querySelector('.input-chat-area').appendChild(newDiv);
+    }
+    }
+});
+
+
